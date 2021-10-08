0b99263a6f222b184895cd3bb8d283ceb9ae89e7
clarelimo <clare.limo@student.moringaschool.com>
2021-10-08 13:08:00 +0300

    categories added

---
+++ app/src/main/java/com/moringa/automated_donation_platform_android/SignupActivity.java
@@ -0,0 +1,52 @@
+package com.moringa.automated_donation_platform_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinner;
+    @BindView(R.id.spinner) Spinner mSpinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        List<String> categories = new ArrayList<>();
+        categories.add(0,"Choose Category");
+        categories.add("Admin");
+        categories.add("Donor");
+        categories.add("Charity");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        if (parent.getItemAtPosition(position).equals("Choose Category")){
+            //Do anything
+        }else{
+            String item = parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+}
