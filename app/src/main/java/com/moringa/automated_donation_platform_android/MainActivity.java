de3f93b3be04de99ac353a3ccd818bc09dd72b86
clarelimo <clare.limo@student.moringaschool.com>
2021-10-08 10:49:25 +0300

    link main activity to login page

--- app/src/main/java/com/moringa/automated_donation_platform_android/MainActivity.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/MainActivity.java
@@ -2,13 +2,27 @@ package com.moringa.automated_donation_platform_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.loginPageBtn) Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
