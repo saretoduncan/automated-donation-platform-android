ceb2a2b762da751ddef0360ba1e58e15763dc5db
clarelimo <clare.limo@student.moringaschool.com>
2021-10-10 16:53:15 +0300

    linked login page and charity page

--- app/src/main/java/com/moringa/automated_donation_platform_android/LoginActivity.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/LoginActivity.java
@@ -2,13 +2,30 @@ package com.moringa.automated_donation_platform_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.loginButton) Button mLoginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLoginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginBtn) {
+            Intent intent = new Intent(LoginActivity.this, CharityActivity.class);
+            startActivity(intent);
+        }
     }
 }
