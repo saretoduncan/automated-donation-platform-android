0b3e3cfee973276b2b5e60b008fb3dbf51d86612
clarelimo <clare.limo@student.moringaschool.com>
2021-10-10 19:21:40 +0300

    donor list item view added

---
+++ app/src/main/java/com/moringa/automated_donation_platform_android/DonorListAdapter.java
@@ -0,0 +1,24 @@
+package com.moringa.automated_donation_platform_android;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
