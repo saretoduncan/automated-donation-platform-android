7d90f7969c1254932202004c2698d9865ff25e2d
clarelimo <clare.limo@student.moringaschool.com>
2021-10-10 16:10:53 +0300

    add bottom nav in charity activity

---
+++ app/src/main/java/com/moringa/automated_donation_platform_android/CharityActivity.java
@@ -0,0 +1,14 @@
+package com.moringa.automated_donation_platform_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CharityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity);
+    }
+}
