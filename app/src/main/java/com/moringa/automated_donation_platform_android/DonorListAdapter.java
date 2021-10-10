31724aea85dc2628f5872b98bea1a02190bd5212
clarelimo <clare.limo@student.moringaschool.com>
2021-10-10 19:52:13 +0300

    donor list adapter aded

--- app/src/main/java/com/moringa/automated_donation_platform_android/DonorListAdapter.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/DonorListAdapter.java
@@ -1,24 +1,56 @@
 package com.moringa.automated_donation_platform_android;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class DonorListAdapter extends RecyclerView.Adapter<DonorListAdapter.DonorsViewHolder> {
+    List<String> names;
+    List<Integer> amount;
+    Context context;
+
+    public DonorListAdapter(Context context, List<String> names, List<Integer> amount) {
+        this.names = names;
+        this.amount = amount;
+        this.context = context;
+    }
+
     @NonNull
     @Override
-    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public DonorListAdapter.DonorsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.donor_list_item,parent,false);
+        DonorsViewHolder viewHolder = new DonorsViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull DonorsViewHolder holder, int position) {
+//        holder.donorAmountTxt.setText();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return names.size();
+    }
+
+    public class DonorsViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.nameTextView) TextView nameTxt;
+        @BindView(R.id.donorAmountTextView) TextView donorAmountTxt;
+        @BindView(R.id.donorProfilePic) CircleImageView donorImage;
+        public DonorsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
     }
 }
--- app/src/main/java/com/moringa/automated_donation_platform_android/HomeFragment.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/HomeFragment.java
@@ -3,12 +3,21 @@ package com.moringa.automated_donation_platform_android;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeFragment extends Fragment {
+    @BindView(R.id.donorList) RecyclerView donorList;
+    List<String> names;
+    List<Integer> amount;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -27,6 +36,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ButterKnife.bind(this);
 
     }
 
