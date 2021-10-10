49842e0e1b6806dc0c2bd85738cc79d512c64d1a
clarelimo <clare.limo@student.moringaschool.com>
2021-10-10 16:49:16 +0300

    navigate to selected item

--- app/src/main/java/com/moringa/automated_donation_platform_android/CharityActivity.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/CharityActivity.java
@@ -1,14 +1,49 @@
 package com.moringa.automated_donation_platform_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CharityActivity extends AppCompatActivity {
 
+    @BindView(R.id.bottomNavigation) BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity);
+        ButterKnife.bind(this);
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.navHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navBeneficiaries:
+                    selectedFragment = new BeneficiariesFragment();
+                    break;
+                case R.id.navProfile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                    selectedFragment).commit();
+            return true;
+        }
+    };
 }
