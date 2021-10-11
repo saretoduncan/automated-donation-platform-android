c1e388925980945f2aa717590c43018e894d6f50
clarelimo <clare.limo@student.moringaschool.com>
2021-10-11 11:34:03 +0300

    charity profile complete

--- app/src/main/java/com/moringa/automated_donation_platform_android/fragments/BeneficiaryProfileFragment.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/fragments/BeneficiaryProfileFragment.java
@@ -61,6 +61,6 @@ public class BeneficiaryProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beneficiary_profile, container, false);
+        return inflater.inflate(R.layout.fragment_charity_profile, container, false);
     }
 }
