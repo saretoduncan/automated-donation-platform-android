31265afe45afaedfbd7fb0fb9505b5cfad63e8db
clarelimo <clare.limo@student.moringaschool.com>
2021-10-11 08:33:55 +0300

    beneficiary fragment complete

--- app/src/main/java/com/moringa/automated_donation_platform_android/fragments/BeneficiariesFragment.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/fragments/BeneficiariesFragment.java
@@ -3,64 +3,68 @@ package com.moringa.automated_donation_platform_android.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.moringa.automated_donation_platform_android.R;
+import com.moringa.automated_donation_platform_android.adapters.BeneficiaryListAdapter;
+import com.moringa.automated_donation_platform_android.adapters.DonorListAdapter;
+import com.moringa.automated_donation_platform_android.models.Beneficiary;
+import com.moringa.automated_donation_platform_android.models.Donor;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BeneficiariesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeneficiariesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView mRecyclerView;
+    List<Beneficiary> mBeneficiaries;
 
     public BeneficiariesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BeneficiariesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BeneficiariesFragment newInstance(String param1, String param2) {
-        BeneficiariesFragment fragment = new BeneficiariesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mBeneficiaries = new ArrayList<>();
+        mBeneficiaries.add(new Beneficiary("Nelly Nayoma","“I can now concentrate in class, I find no reason to miss\n" +
+                " classes during my periods, I can stand in front of my \n" +
+                "classmates and solve a math problem, I have developed a love\n" +
+                " for soccer, and I can do all of this without thinking of my \n" +
+                "period when they arrive,” ", R.drawable.pic1));
+        mBeneficiaries.add(new Beneficiary("Akura rakera","“I can now concentrate in class, I find no reason to miss\n" +
+                " classes during my periods, I can stand in front of my \n" +
+                "classmates and solve a math problem, I have developed a love\n" +
+                " for soccer, and I can do all of this without thinking of my \n" +
+                "period when they arrive,” ",R.drawable.pic2));
+        mBeneficiaries.add(new Beneficiary("Clare Limo","“I can now concentrate in class, I find no reason to miss\n" +
+                " classes during my periods, I can stand in front of my \n" +
+                "classmates and solve a math problem, I have developed a love\n" +
+                " for soccer, and I can do all of this without thinking of my \n" +
+                "period when they arrive,” ",R.drawable.pic3));
+        mBeneficiaries.add(new Beneficiary("Duncan Moiyo","“I can now concentrate in class, I find no reason to miss\n" +
+                " classes during my periods, I can stand in front of my \n" +
+                "classmates and solve a math problem, I have developed a love\n" +
+                " for soccer, and I can do all of this without thinking of my \n" +
+                "period when they arrive,” ",R.drawable.pic4));
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beneficiaries, container, false);
+        View view = inflater.inflate(R.layout.fragment_beneficiaries, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.beneficiaryList);
+        BeneficiaryListAdapter mAdapter = new BeneficiaryListAdapter(mBeneficiaries, getContext());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
     }
 }
