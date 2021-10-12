28c5bece3e8fba0a2528c0ca4b4f9db7a26506f4
clarelimo <clare.limo@student.moringaschool.com>
2021-10-12 15:23:16 +0300

    splash screen complete

---
+++ app/src/main/java/com/moringa/automated_donation_platform_android/ui/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.moringa.automated_donation_platform_android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.moringa.automated_donation_platform_android.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
