d5a209d16c13f4c219911c4682a061a2ff98db35
clarelimo <clare.limo@student.moringaschool.com>
2021-10-12 17:56:35 +0300

    change from signup to charity approval fragment

--- app/src/main/java/com/moringa/automated_donation_platform_android/ui/LoginActivity.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/ui/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.moringa.automated_donation_platform_android.R;
 
@@ -14,6 +15,7 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.loginButton) Button mLoginBtn;
+    @BindView(R.id.registerTextView) TextView mSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +23,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         ButterKnife.bind(this);
 
         mLoginBtn.setOnClickListener(this);
+        mSignup.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == mLoginBtn) {
             Intent intent = new Intent(LoginActivity.this, CharityActivity.class);
+            startActivity(intent);
+        }
+
+        if (v == mSignup) {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
             startActivity(intent);
         }
     }
--- app/src/main/java/com/moringa/automated_donation_platform_android/ui/SignupActivity.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/ui/SignupActivity.java
@@ -1,6 +1,9 @@
 package com.moringa.automated_donation_platform_android.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +12,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.moringa.automated_donation_platform_android.R;
+import com.moringa.automated_donation_platform_android.fragments.CallbackFragment;
+import com.moringa.automated_donation_platform_android.fragments.CharityApprovalFragment;
+import com.moringa.automated_donation_platform_android.fragments.HomeFragment;
+import com.moringa.automated_donation_platform_android.fragments.SignupFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,38 +23,69 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class SignupActivity extends AppCompatActivity implements CallbackFragment {
 
-    @BindView(R.id.spinner) Spinner spinner;
+//    @BindView(R.id.spinner) Spinner spinner;
+    Fragment fragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
 
-        List<String> categories = new ArrayList<>();
-        categories.add(0,"Choose Category");
-        categories.add("Admin");
-        categories.add("Donor");
-        categories.add("Charity");
+        addFragment();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,categories);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+//        List<String> categories = new ArrayList<>();
+//        categories.add(0,"Choose Category");
+//        categories.add("Admin");
+//        categories.add("Donor");
+//        categories.add("Charity");
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,categories);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(this);
+    }
+
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+//        if (parent.getItemAtPosition(position).equals("Choose Category")){
+//            //Do anything
+//        }else{
+//            String item = parent.getItemAtPosition(position).toString();
+//            if (item.equals("Charity")){
+//
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//        // TODO Auto-generated method stub
+//    }
+
+    public void addFragment(){
+        SignupFragment fragment = new SignupFragment();
+        fragment.setCallbackFragment(this);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.signupFragment,fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void replaceFragment(){
+        fragment = new CharityApprovalFragment();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.signupFragment,fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-        if (parent.getItemAtPosition(position).equals("Choose Category")){
-            //Do anything
-        }else{
-            String item = parent.getItemAtPosition(position).toString();
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // TODO Auto-generated method stub
+    public void changeFragment() {
+        replaceFragment();
     }
 }
