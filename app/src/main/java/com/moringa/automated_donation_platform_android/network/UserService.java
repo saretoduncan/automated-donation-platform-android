f2bbd7073874d86d8bc5bc3d318a555037abfb22
clarelimo <clare.limo@student.moringaschool.com>
2021-10-15 12:44:21 +0300

    login models

--- app/src/main/java/com/moringa/automated_donation_platform_android/network/UserService.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/network/UserService.java
@@ -1,7 +1,17 @@
 package com.moringa.automated_donation_platform_android.network;
 
+import com.moringa.automated_donation_platform_android.models.LoginRequest;
+import com.moringa.automated_donation_platform_android.models.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface UserService {
+
+    @POST("authenticate/login")
+    Call<LoginResponse> userLogin(@Body LoginRequest loginRequest);
+
 
 }
