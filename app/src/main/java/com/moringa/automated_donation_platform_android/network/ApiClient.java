04991463bf3fa9a0472c9945cbb7321f2d6e2a92
clarelimo <clare.limo@student.moringaschool.com>
2021-10-15 12:59:18 +0300

    get user service on apiclient

--- app/src/main/java/com/moringa/automated_donation_platform_android/network/ApiClient.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/network/ApiClient.java
@@ -1,19 +1,31 @@
 package com.moringa.automated_donation_platform_android.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
-
     public static final String BASE_URL = "https://water-records-api.herokuapp.com/";
-    public static UserService getClient(){
+    public static Retrofit getRetrofit(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
         if(retrofit == null){
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
-        return retrofit.create(UserService.class);
+        return retrofit;
+    }
+
+    public static UserService getUserService(){
+        UserService userService = getRetrofit().create(UserService.class);
+        return userService;
     }
 }
