ee2e58e45693bf949af2ca58389c70bc6e45c0ce
saretoduncan <saretoduncan@gmail.com>
2021-10-15 06:59:33 +0300

    add admin_toolbar

---
+++ app/src/main/java/com/moringa/automated_donation_platform_android/ui/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.automated_donation_platform_android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringa.automated_donation_platform_android.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
