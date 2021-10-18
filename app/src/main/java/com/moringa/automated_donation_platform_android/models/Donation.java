ba095225b5cda0457b1505011bfe8d0de5d13fc0
saretoduncan <saretoduncan@gmail.com>
2021-10-18 11:36:02 +0300

    generate models for donations

--- app/src/main/java/com/moringa/automated_donation_platform_android/models/Donation.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/models/Donation.java
@@ -1,4 +1,78 @@
 package com.moringa.automated_donation_platform_android.models;
 
+import java.security.Timestamp;
+
 public class Donation {
+    private int userid;
+    private int charityid;
+    private boolean anonymity;
+    private String frequency;
+    private Timestamp reminderdate;
+    private String paymentmode;
+    private int id;
+    public Donation(int userid, int charityid, boolean anonymity, String frequency, Timestamp reminderdate, String paymentmode, int id) {
+        this.userid = userid;
+        this.charityid = charityid;
+        this.anonymity = anonymity;
+        this.frequency = frequency;
+        this.reminderdate = reminderdate;
+        this.paymentmode = paymentmode;
+        this.id = id;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getCharityid() {
+        return charityid;
+    }
+
+    public void setCharityid(int charityid) {
+        this.charityid = charityid;
+    }
+
+    public boolean isAnonymity() {
+        return anonymity;
+    }
+
+    public void setAnonymity(boolean anonymity) {
+        this.anonymity = anonymity;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public Timestamp getReminderdate() {
+        return reminderdate;
+    }
+
+    public void setReminderdate(Timestamp reminderdate) {
+        this.reminderdate = reminderdate;
+    }
+
+    public String getPaymentmode() {
+        return paymentmode;
+    }
+
+    public void setPaymentmode(String paymentmode) {
+        this.paymentmode = paymentmode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
