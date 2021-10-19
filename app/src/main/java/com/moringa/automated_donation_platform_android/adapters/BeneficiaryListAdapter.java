1e1dfbf3e52be390de2e4235155c3058274e3c9c
clarelimo <clare.limo@student.moringaschool.com>
2021-10-19 12:30:48 +0300

    display image

--- app/src/main/java/com/moringa/automated_donation_platform_android/adapters/BeneficiaryListAdapter.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/adapters/BeneficiaryListAdapter.java
@@ -1,18 +1,28 @@
 package com.moringa.automated_donation_platform_android.adapters;
 
+import static com.cloudinary.android.ResponsiveUrl.Preset.FIT;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cloudinary.android.MediaManager;
+import com.cloudinary.android.download.DownloadRequestCallback;
 import com.moringa.automated_donation_platform_android.R;
 import com.moringa.automated_donation_platform_android.models.Beneficiary;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import butterknife.BindView;
@@ -41,7 +51,12 @@ public class BeneficiaryListAdapter extends RecyclerView.Adapter<BeneficiaryList
         holder.nameTxt.setText(mBeneficiary.get(position).getName());
         holder.testimonialTxt.setText(mBeneficiary.get(position).getTestimony());
         String imageUrl = mBeneficiary.get(position).getImage();
-        Picasso.get().load(imageUrl).into(holder.beneficiaryImage);
+//        Picasso.get().load(imageUrl).into(holder.beneficiaryImage);
+        try {
+            imageDisplay(imageUrl,holder.beneficiaryImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -58,5 +73,11 @@ public class BeneficiaryListAdapter extends RecyclerView.Adapter<BeneficiaryList
             super(itemView);
             ButterKnife.bind(this,itemView);
         }
+    }
+
+    public void imageDisplay(String url, ImageView profile_photo) throws IOException {
+        URL newurl = new URL(url);
+        Bitmap mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
+        profile_photo.setImageBitmap(mIcon_val);
     }
 }
