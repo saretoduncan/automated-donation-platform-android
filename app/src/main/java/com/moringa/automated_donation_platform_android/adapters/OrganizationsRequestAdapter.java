9d5bbb5ef496e5fb97470e9c69260e78154ade23
saretoduncan <saretoduncan@gmail.com>
2021-10-24 13:17:32 +0300

    add approve and cancel approvals functionality

--- app/src/main/java/com/moringa/automated_donation_platform_android/adapters/OrganizationsRequestAdapter.java
+++ app/src/main/java/com/moringa/automated_donation_platform_android/adapters/OrganizationsRequestAdapter.java
@@ -1,10 +1,12 @@
 package com.moringa.automated_donation_platform_android.adapters;
 
 import android.annotation.SuppressLint;
+import android.util.JsonToken;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,8 +16,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.moringa.automated_donation_platform_android.R;
 import com.moringa.automated_donation_platform_android.models.Admin;
 import com.moringa.automated_donation_platform_android.models.Charity;
+import com.moringa.automated_donation_platform_android.models.User;
 import com.moringa.automated_donation_platform_android.network.ApiClient;
 import com.moringa.automated_donation_platform_android.viewModel.AdminViewModel;
+import com.moringa.automated_donation_platform_android.viewModel.UserViewModelCharityListVIewModel;
 
 import org.w3c.dom.Text;
 
@@ -52,6 +56,23 @@ public class OrganizationsRequestAdapter extends RecyclerView.Adapter<Organizati
                 if(response.isSuccessful()) {
                     System.out.println("charityResponse::: was a success");
                     holder.aboutOrg.setText(response.body().getDescription());
+                    int userId= response.body().getUserId();
+                  
+                    Call<User> userCall = ApiClient.getUserService().getUserDetails(userId);
+                    userCall.enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            if (response.isSuccessful()) {
+                                System.out.println("user.request::: is a success");
+                                holder.organisationName.setText(response.body().getName());
+                            }else System.out.println("user.request::: is not a success");
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+
+                        }
+                    });
 
                 }else System.out.println("charityResponse:::: was not a success");
 
@@ -62,7 +83,44 @@ public class OrganizationsRequestAdapter extends RecyclerView.Adapter<Organizati
 
             }
         });
+        holder.approve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<Admin> approve = ApiClient.getAdminServices().adminApproveCharity(charityId);
+                approve.enqueue(new Callback<Admin>() {
+                    @Override
+                    public void onResponse(Call<Admin> call, Response<Admin> response) {
+                        if(response.isSuccessful()){
+                            Toast.makeText(view.getContext(), "approval success", Toast.LENGTH_SHORT).show();
+                        }else Toast.makeText(view.getContext(), "approval not a success", Toast.LENGTH_SHORT).show();
+                    }
 
+                    @Override
+                    public void onFailure(Call<Admin> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+       holder.cancelApproval.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Call<Void> cancelRequest = ApiClient.getAdminServices().adminDeleteCharityOrganisation(charityId);
+               cancelRequest.enqueue(new Callback<Void>() {
+                   @Override
+                   public void onResponse(Call<Void> call, Response<Void> response) {
+                       if(response.isSuccessful()){
+                           Toast.makeText(view.getContext(), "delete is a success", Toast.LENGTH_SHORT).show();
+                       }else Toast.makeText(view.getContext(), "delete is not success", Toast.LENGTH_SHORT).show();
+                   }
+
+                   @Override
+                   public void onFailure(Call<Void> call, Throwable t) {
+
+                   }
+               });
+           }
+       });
     }
 
     @Override
@@ -77,6 +135,8 @@ public class OrganizationsRequestAdapter extends RecyclerView.Adapter<Organizati
         @SuppressLint("NonConstantResourceId")
         @BindView(R.id.organization_about)
         TextView aboutOrg;
+        @SuppressLint("NonConstantResourceId")
+        @BindView(R.id.organization_Name) TextView organisationName;
         @SuppressLint("NonConstantResourceId")
         @BindView(R.id.accept) TextView approve;
         @SuppressLint("NonConstantResourceId")
